b49cafbeff96fbe0863966faac9bac1a189b23a4
Christopher Ryan <christopherr@missionreadyhq.com>
2024-12-03 09:06:07 +1300

    practice package.json updated

--- practice/server.js
+++ practice/server.js
@@ -1,8 +1,15 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
+import dotenv from "dotenv";
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+dotenv.config({ path: resolve(__dirname, '../.env') });
+
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-const prompt = "Write a story about a magic backpack.";
+const prompt = "Write a story about a magic backpack. in 50 words";
 
 const result = await model.generateContent(prompt);
 console.log(result.response.text());
