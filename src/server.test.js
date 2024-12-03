89822674f6be029915c2c20583c1de6d8079cdfa
Cameron McEwing <cameronm@missionreadyhq.com>
2024-12-03 22:01:53 +1300

    removed commented out code, and unused files, still not sure if we need ./src/.env as everthing seems to use ./.env now. I have left it until I'm sure. update readme with usage instructions for the code.

--- src/server.test.js
+++ src/server.test.js
@@ -35,38 +35,3 @@ describe('Test API endpoints', () => {
 });
 
 
-
-// const request = require('supertest');
-// const server = require('./server');
-// let app;
-
-// describe('Test API endpoints', () => {
-//   test('POST /api/start', async () => {
-//     const response = await request(server)
-//       .post('/api/start')
-//       .send({ jobTitle: 'Software Developer' });
-//     expect(response.status).toBe(200);
-//   });
-
-//   test('POST /api/respond', async () => {
-//     const response = await request(server)
-//       .post('/api/respond')
-//       .send({
-//         jobTitle: 'Software Developer',
-//         response: 'I have 5 years of experience',
-//         history: []
-//       });
-//     expect(response.status).toBe(200);
-//   });
-
-//   test('POST /api/analyse', async () => {
-//     const response = await request(server)
-//       .post('/api/analyse')
-//       .send({
-//         jobTitle: 'Software Developer',
-//         history: []
-//       });
-//     expect(response.status).toBe(200);
-//   });
-// });
-
